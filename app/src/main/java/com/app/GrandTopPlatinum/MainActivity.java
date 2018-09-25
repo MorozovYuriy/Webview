d77d86d8edbff82e0f03e3c396657f8fa3901be5
MorozovYuriy <morozovyuriy11@gmail.com>
2018-09-25 15:07:58 +0300

    first commit

---
+++ app/src/main/java/com/app/GrandTopPlatinum/MainActivity.java
@@ -0,0 +1,40 @@
+package com.app.GrandTopPlatinum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("your URL");
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
